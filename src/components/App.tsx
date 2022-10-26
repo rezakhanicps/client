f8a8914f8b877959b7f5114bfe98686c24c00ded
reza khani <43088060+rezakhani1376@users.noreply.github.com>
2022-10-26 11:06:46 +0330

    Handle Routes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,18 @@
-import { BrowserRouter, Route, Router } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import StreamCreate from './streams/stream-create';
+import StreamDelete from './streams/stream-delete';
+import StreamEdit from './streams/stream-edit';
+import StreamList from './streams/stream-list';
+import StreamShow from './streams/stream-show';
 const App = () => {
     return (
         <div>
             <BrowserRouter>
-                {/* <Route path="/" exact component={} /> */}
+                <Route path="/" exact component={StreamList} />
+                <Route path="/streams/new" exact component={StreamCreate} />
+                <Route path="/streams/edit" exact component={StreamEdit} />
+                <Route path="/streams/delete" exact component={StreamDelete} />
+                <Route path="/streams/show" exact component={StreamShow} />
             </BrowserRouter>
         </div>
     );
