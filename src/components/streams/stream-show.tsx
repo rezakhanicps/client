d7a9c8ef75683241bad1b0041c503df6196654a1
reza khani <43088060+rezakhani1376@users.noreply.github.com>
2022-10-27 19:52:42 +0330

    Video Player Setup

--- src/components/streams/stream-show.tsx
+++ src/components/streams/stream-show.tsx
@@ -1,5 +1,48 @@
-const StreamShow = () => {
-    return <div>StreamShow</div>;
+import { useEffect, useRef } from 'react';
+import flv from 'flv.js';
+import { useAction } from '../../hooks/use-actions';
+import { useTypedSelector } from '../../hooks/use-typed-selector';
+
+const StreamShow: React.FC<any> = (props) => {
+    const { id } = props.match.params;
+    const { fetchStream } = useAction();
+    const videoRef = useRef<any>(null);
+    //@ts-ignore
+    const stream = useTypedSelector((state) => state.streams[id]);
+
+    let player: any;
+    useEffect(() => {
+        fetchStream(id);
+        buildPlayer();
+    }, []);
+
+    useEffect(() => {
+        buildPlayer();
+    });
+
+    const buildPlayer = () => {
+        if (player || !stream) {
+            return;
+        }
+        player = flv.createPlayer({
+            type: 'flv',
+            url: `http://localhost:8000/live/${id}.flv`,
+        });
+        player.attachMediaElement(videoRef.current);
+        player.load();
+    };
+
+    if (!stream) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <div>
+            <video ref={videoRef} style={{ width: '100%' }} controls />
+            <h1>{stream.title}</h1>
+            <h5>{stream.description}</h5>
+        </div>
+    );
 };
 
 export default StreamShow;
