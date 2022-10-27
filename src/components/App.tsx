c37b13f6524b334a3ec255c0702b21ff74f0d787
reza khani <43088060+rezakhani1376@users.noreply.github.com>
2022-10-27 18:10:13 +0330

    Switch With React

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,22 +1,24 @@
-import { Router, Route } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 import history from '../history';
-// @ts-ignore
 import Header from './header';
 import StreamCreate from './streams/stream-create';
 import StreamDelete from './streams/stream-delete';
 import StreamEdit from './streams/stream-edit';
 import StreamList from './streams/stream-list';
 import StreamShow from './streams/stream-show';
+
 const App = () => {
     return (
         <div className="ui container">
             <Router history={history}>
                 <Header />
-                <Route path="/" exact component={StreamList} />
-                <Route path="/streams/new" exact component={StreamCreate} />
-                <Route path="/streams/edit/:id" exact component={StreamEdit} />
-                <Route path="/streams/delete/:id" exact component={StreamDelete} />
-                <Route path="/streams/show" exact component={StreamShow} />
+                <Switch>
+                    <Route path="/" exact component={StreamList} />
+                    <Route path="/streams/new" exact component={StreamCreate} />
+                    <Route path="/streams/edit/:id" exact component={StreamEdit} />
+                    <Route path="/streams/delete/:id" exact component={StreamDelete} />
+                    <Route path="/streams/:id" exact component={StreamShow} />
+                </Switch>
             </Router>
         </div>
     );
